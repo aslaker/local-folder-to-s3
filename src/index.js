89b061cc801dbec855668a87caa0e18560e4ad8d
Adam Slaker <adamslaker@gmail.com>
2020-03-19 18:32:48 -0500

    Updated code to handle upload to s3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const AWS = require("aws-sdk");
 const fs = require("fs");
 require("dotenv").config();
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const bucketName = "mining-game-assets";
 
 const watchedFilesPath = `${__dirname}/files`;
 
@@ -11,6 +16,41 @@ if (!fs.existsSync(watchedFilesPath)) {
   fs.mkdirSync(watchedFilesPath);
 }
 
-watch(watchedFilesPath, { recursive: true }, (evt, name) => {
-  console.log("%s changed", name);
+watch(watchedFilesPath, { recursive: true }, (evt, filePath) => {
+
+  //Get the filename
+  const fileName = filePath.replace(/^.*[\\\/]/, "");
+
+  // On File Add or Update
+  if (evt === "update") {
+    fs.readFile(filePath, (err, data) => {
+      if (err) throw err;
+
+      const params = {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: JSON.stringify(data, null, 2)
+      };
+
+      s3.upload(params, (s3Err, data) => {
+        console.log("UPLOADING...")
+        if (s3Err) throw s3Err;
+        console.log(`File Uploaded Successfully at ${data.Location}`);
+      });
+    });
+  }
+
+  // On File Remove
+  if (evt === "remove") {
+    const params = {
+      Bucket: bucketName,
+      Key: fileName
+    };
+
+    s3.deleteObject(params, (s3Err, data) => {
+      console.log("DELETING...")
+      if (s3Err) throw s3Err;
+      console.log(`Successfully deleted ${data.Location}`);
+    });
+  }
 });
